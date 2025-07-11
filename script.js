f08aaef45547d0935a9866b42b17a5c2bfbb7d55
pvbsovsv <pablo.a.sousa@gmail.com>
2025-07-11 22:35:50 +0100

    js edit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const express = require('express')
+const express = req('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   
-    res.send('Hello World!')
+    res.send(console.log("works!"))
 })
 
 app.listen(port, () => {
