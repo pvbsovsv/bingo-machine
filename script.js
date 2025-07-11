8a3c48dd851ae8b6426f69574f2ac6a7b33e5364
pvbsovsv <pablo.a.sousa@gmail.com>
2025-07-11 23:12:37 +0100

    trial fix

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
-const app = express()
-const port = 4000
 
-app.get('/', (req, res) => {
-  
-    
-    fetch('https://bingo-servidor.onrender.com/frases')
-    .then(res = res.json())
-    .then(frases => {
+// fetch frases do servidor
 
-        frases.forEach(frase => {
-
-            console.log(frase)
-
-        })
-
+fetch('https://bingo-servidor.onrender.com/frases')
+  .then(res => res.json())
+  .then(frases => {
+    frases.forEach(frase => {
+      console.log(frase) // test
     })
-    
-    
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  })
+  .catch(error => {
+    console.error('Error fetching frases:', error)
+  })
