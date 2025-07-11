4974dde55eca7908bc74b6c2d770b673081ec06f
pvbsovsv <pablo.a.sousa@gmail.com>
2025-07-11 22:59:07 +0100

    try frases 1

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-const express = required('express')
-const app = express()
-const port = 4000
 
 app.get('/', (req, res) => {
   
     
-    fetch('https://bingo-servidor.onrender.com/')
+    fetch('https://bingo-servidor.onrender.com/frases')
+    .then(res = res.json())
+    .then(frases => {
+
+        frases.forEach(frase => {
+
+            console.log(frase)
+
+        })
+
+    })
     
     
-    res.send(console.log("server online"))
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
